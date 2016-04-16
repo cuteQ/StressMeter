81c5ffb876fd4da5b14f0a3efb86545cf5a784d0
illylli <lilly.lly@hotmail.com>
2016-04-16 13:12:41 -0400

    Some error

--- app/src/main/java/com/example/yuzhong/stressmeter/MainActivity.java
+++ app/src/main/java/com/example/yuzhong/stressmeter/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.yuzhong.stressmeter;
 
+import android.media.MediaPlayer;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -15,13 +15,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+   public MediaPlayer alarmMusic;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playMusic();
         // Set a Toolbar to replace the ActionBar.
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -36,10 +39,41 @@ public class MainActivity extends AppCompatActivity
         //this is setup the navigation view and
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+<<<<<<< HEAD
+
+        //set default selection to stressmeter!
+        Fragment fragment = null;
+        Class fragmentClass = null;
+        fragmentClass = StressMeterFragment.class;
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+//
+        MenuItem item = navigationView.getMenu().getItem(0);
+
+        // Highlight the selected item has been done by NavigationView
+        item.setChecked(true);
+        // Set action bar title
+        setTitle(item.getTitle());
+
+=======
+>>>>>>> parent of 8c31e11... Yuzhong_First_Version
     }
+
+
+
 
     @Override
     public void onBackPressed() {
+        if ( alarmMusic!= null) {
+            alarmMusic.stop();
+            alarmMusic.release();
+        }
         //if navigation view is start, then close navigation view!
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
@@ -47,8 +81,28 @@ public class MainActivity extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
+
     }
 
+    public void playMusic(){
+
+        Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+
+
+        // setContentView(R.layout.activity_alarm);
+
+        try {
+            alarmMusic = new MediaPlayer();
+            alarmMusic.setDataSource(this, uri);
+            alarmMusic.setLooping(true);
+            alarmMusic.prepare();
+            alarmMusic.start();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -61,6 +115,10 @@ public class MainActivity extends AppCompatActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        if ( alarmMusic!= null) {
+            alarmMusic.stop();
+            alarmMusic.release();
+        }
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -74,6 +132,10 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+        if ( alarmMusic!= null) {
+            alarmMusic.stop();
+            alarmMusic.release();
+        }
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
@@ -108,4 +170,8 @@ public class MainActivity extends AppCompatActivity
 
         return true;
     }
+
+
+
+
 }
